4cf1e33912131e47147ec06f8fb191b4de6ba688
AJAYK-01 <ajayk@cet.ac.in>
2021-01-10 20:12:23 +0530

    server code update

--- ytb-dl-backend/server.js
+++ ytb-dl-backend/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 const cors = require('cors');
 const { exec } = require('child_process');
 const contentDisposition = require('content-disposition');
@@ -7,9 +8,18 @@ const app = express();
 
 app.use(cors());
 
-const prcs = exec('pip3 install youtube-dl');
+const publicPath = path.join('.', 'build');
+const port = process.env.PORT || 4000;
 
-app.listen(4000, () => {
+app.use(express.static(publicPath));
+
+// const prcs = exec('pip install --upgrade youtube-dl');
+
+app.get('/', (req, res) => {
+   res.sendFile(path.join(publicPath, 'index.html'));
+});
+
+app.listen(port, () => {
     console.log('Server Works !!! At port 4000');
 });
 app.get('/download', (req,res) => {
@@ -26,8 +36,10 @@ app.get('/download', (req,res) => {
 
   res.header('Content-Disposition', contentDisposition(title)+ext+'"');
 
+  title = URL.split('/').slice(-1);
+
   if(filter === 'audioonly') {
-    const command = 'youtube-dl -o \''+'./cache/'+title+'.mp3\' -f bestaudio[ext=m4a]/mp3'+' \''+URL+'\'';
+    const command = 'youtube-dl -o \''+'./cache/'+title+'.mp3\' -f \'bestaudio[ext=m4a]/mp3\''+' \''+URL+'\'';
     console.log(command);
     const process = exec(command);
     
@@ -40,7 +52,7 @@ app.get('/download', (req,res) => {
     })
   }
   else {
-    const command = 'youtube-dl -o \''+'./cache/'+title+'.mp4\' -f '+quality+'[ext=mp4]+bestaudio[ext=m4a]/mp4'+' \''+URL+'\'';
+    const command = 'youtube-dl -o \''+'./cache/'+title+'.mp4\' -f '+quality+'\'[ext=mp4]+bestaudio[ext=m4a]/mp4\''+' \''+URL+'\'';
     console.log(command);
     const process = exec(command);
     
